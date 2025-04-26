f912e68f511a80c6cfe720a9459a63f28586ac8b
Thanh-Qui <quiluong111@gmail.com>
2025-04-26 17:05:58 +0700

    part 13

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -7,13 +7,25 @@ import Slider from "react-slick";
 
 
 import ProductDetails1 from '../../assets/images/product-16-2.jpg';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
+import { Button } from "@mui/material";
 
+
+import SideBar from "../../components/Sidebar";
 
 const DetailsPage = () => {
 
     const [zoomImage, setZoomImage] = useState(ProductDetails1);
     const [bigImageSize, setBigImageSize] = useState([1500, 1500]);
     const [SmlImageSize, setSmlImageSize] = useState([150, 150]);
+
+    const [activeSize, setActiveSize] = useState(0);
+
+    const [inputValue, setinputValue] = useState(1);
 
     const zoomSlider = useRef();
 
@@ -27,8 +39,24 @@ const DetailsPage = () => {
         arrows: true,
     };
 
-    const goto = (index) => {
-        
+    // click vào chuyển image
+    const goto = (url) => {
+        setZoomImage(url);
+    }
+
+    // chức năng nút chọn size (g)
+    const isActive = (index) => {
+        setActiveSize(index);
+    }
+
+    const plus = () => {
+        setinputValue(inputValue+1);
+    }
+
+    const minus = () => {
+        if(inputValue!==1) {
+            setinputValue(inputValue-1);
+        }
     }
 
     return (
@@ -50,37 +78,37 @@ const DetailsPage = () => {
                             {/* product zoom code start here */}
                             <div className="col-md-5">
                                 <div className="productZoom">
-                                    <InnerImageZoom src={`${ProductDetails1}?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} zoomType="hover" zoomScale={1} />
+                                    <InnerImageZoom src={`${zoomImage}?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} zoomType="hover" zoomScale={1} />
                                 </div>
 
                                 <Slider {...settings} className="zoomSlider" ref={zoomSlider}>
                                     <div className="item">
                                         <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(0)}/>
+                                        onClick={()=>goto(ProductDetails1)}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(1)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-3.jpg')}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(2)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-4.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-4.jpg')}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(3)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-5.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-5.jpg')}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(4)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-6.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-6.jpg')}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(5)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-7.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-7.jpg')}/>
                                     </div>
                                     <div className="item">
-                                        <img src={`${ProductDetails1}?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
-                                        onClick={()=>goto(6)}/>
+                                        <img src={`https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-8.jpg?im=Resize=(${SmlImageSize[0]},${SmlImageSize[1]})`} alt="" className="w-100" 
+                                        onClick={()=>goto('https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/thumbnail-8.jpg')}/>
                                     </div>
                                 </Slider>
                                 
@@ -95,7 +123,7 @@ const DetailsPage = () => {
                                     <span className="text-light">(32 reviews)</span>
                                 </div>
 
-
+                                {/* Price */}
                                 <div className="priceSec d-flex align-items-center">
                                     <span className="text-g priceLarge">$38</span>
                                     <div className="ms-2 d-flex flex-column">
@@ -104,16 +132,43 @@ const DetailsPage = () => {
                                     </div>
                                 </div>
 
+                                {/* product details */}
                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam rem officia, 
                                     corrupti reiciendis minima nisi modi, quasi, odio minus dolore impedit fuga eum eligendi.</p>
+
+
+                                {/* Size / Weight: */}
+                                <div className="productSize d-flex align-items-center">
+                                    <span>Size / Weight:</span>
+                                    <ul className="list list-inline mb-0 ps-2">
+                                        <li className="list-inline-item"><a className={`tag ${activeSize===0 ? 'active' : ''}`} onClick={()=>isActive(0)}>50g</a></li>
+                                        <li className="list-inline-item"><a className={`tag ${activeSize===1 ? 'active' : ''}`} onClick={()=>isActive(1)}>60g</a></li>
+                                        <li className="list-inline-item"><a className={`tag ${activeSize===2 ? 'active' : ''}`} onClick={()=>isActive(2)}>80g</a></li>
+                                        <li className="list-inline-item"><a className={`tag ${activeSize===3 ? 'active' : ''}`} onClick={()=>isActive(3)}>100g</a></li>
+                                        <li className="list-inline-item"><a className={`tag ${activeSize===4 ? 'active' : ''}`} onClick={()=>isActive(4)}>150g</a></li>
+                                    </ul>
+                                </div>
+
+
+                                <div className="addCartSection pt-4 pb-4 d-flex align-items-center">
+                                    <div className="counterSec me-2">
+                                        <input type="number" value={inputValue}/>
+                                         <span className="arrow plus" onClick={plus}><KeyboardArrowUpOutlinedIcon/></span>
+                                         <span className="arrow minus" onClick={minus}><KeyboardArrowDownOutlinedIcon/></span>
+                                    </div>
+
+                                    <Button className="btn-g btn-lg addtocartbtn"><ShoppingCartOutlinedIcon/>Add to Cart</Button>
+                                    <Button className="btn-lg addtocartbtn ms-3 btn-border"><FavoriteBorderOutlinedIcon/></Button>
+                                    <Button className="btn-lg addtocartbtn ms-3 btn-border"><CompareArrowsOutlinedIcon/></Button>
+                                </div>
 
                             </div>
                             {/* product info code end here */}
                         </div>
                     </div>
 
-                    <div className="col-md-3 part2">
-
+                    <div className="col-md-3 rightPart sidebarWrapper">
+                        <SideBar/>
                     </div>
 
                 </div>
